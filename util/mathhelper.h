5b58a4acfcd35d2e616c40852548b6ca220a4560
Kenneth Tan <kttan1999@gmail.com>
2020-06-14 21:19:25 +0800

    Kenneth add util files

---
+++ util/mathhelper.h
@@ -0,0 +1,20 @@
+#pragma once
+
+/*
+ * mathhelper.h
+ *
+ *  Version: 6
+ *  Updated: 2 Jun 2020
+ *   Author: Mister_Bander
+ */
+
+namespace mh
+{
+	int max(int a, int b);
+	int max(int a, int b, int c);
+	int irandom(int min, int max);
+	int gcd(int a, int b);
+	float deg(float rad);
+	float sind(float angle);
+	float cosd(float angle);
+}
